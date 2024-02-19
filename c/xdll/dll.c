e41b015fcf5a3ea930ff0a5a49acc6e65c7e8f31
Gregory Maldonado <gmaldona@oswego.edu>
2024-02-19 18:18:46 -0500

    assignments

--- c/xdll/dll.c
+++ c/xdll/dll.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
  * CS 571 Homework 1
  *
@@ -10,6 +13,7 @@
  */
 
 #include "dll.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,18 +25,25 @@
  */
 struct xdll_node *ptxor(struct xdll_node *aptr, struct xdll_node *bptr)
 {
-   return (struct xdll_node *)((uintptr_t)aptr ^ (uintptr_t)bptr);
+    return (struct xdll_node *)((uintptr_t)aptr ^ (uintptr_t)bptr);
 }
 
 /*
  * Swap two pointers within the XDLL.
  * swap + xor => swxorp
  */
-void swxorp(struct xdll_node* aptr, struct xdll_node* bptr)
+void swxorp(struct xdll_node *aptr, struct xdll_node *bptr)
 {
-   aptr = ptxor(aptr, bptr);
-   bptr = ptxor(bptr, aptr); 
-   aptr = ptxor(aptr, bptr);
+    aptr = ptxor(aptr, bptr);
+    bptr = ptxor(bptr, aptr);
+    aptr = ptxor(aptr, bptr);
+}
+
+void swap(struct xdll_node *aptr, struct xdll_node *bptr)
+{
+    aptr = (struct xdll_node *)((uintptr_t)aptr ^ (uintptr_t)aptr);
+    bptr = (struct xdll_node *)((uintptr_t)bptr ^ (uintptr_t)aptr);
+    aptr = (struct xdll_node *)((uintptr_t)aptr ^ (uintptr_t)aptr);
 }
 
 /*
@@ -42,48 +53,47 @@ void swxorp(struct xdll_node* aptr, struct xdll_node* bptr)
  */
 struct xdll_node *new_xdll(size_t size, int32_t max)
 {
-   struct xdll_node *head_pt = NULL;
-   struct xdll_node *curr_pt = NULL;
-   struct xdll_node *prev_pt = NULL;
+    struct xdll_node *head_pt = NULL;
+    struct xdll_node *curr_pt = NULL;
+    struct xdll_node *prev_pt = NULL;
 
-   for (size_t i = 0; i < size; ++i)
-   {
-      int val = rand() % max;
+    for (size_t i = 0; i < size; ++i)
+    {
+        int val = rand() % max;
 
-      if (!curr_pt)
-      {
-         curr_pt = (struct xdll_node *)malloc(sizeof(struct xdll_node));
-         curr_pt->elem = val;
-         head_pt = curr_pt;
-      }
-      else
-      {
-         // assuming curr is already memory allocated...
-         struct xdll_node *next_pt = (struct xdll_node *)malloc(sizeof(struct xdll_node));
-         next_pt->elem = val;
-         next_pt->nxp  = ptxor(curr_pt, NULL);
-         // https://stackoverflow.com/questions/26569728
-         // using-xor-with-pointers-in-c
-         curr_pt->nxp = ptxor(prev_pt, next_pt);
-         prev_pt = curr_pt;
-         curr_pt = next_pt;
-         
-      }
-   }
-   return head_pt;
+        if (!curr_pt)
+        {
+            curr_pt = (struct xdll_node *)malloc(sizeof(struct xdll_node));
+            curr_pt->elem = val;
+            head_pt = curr_pt;
+        }
+        else
+        {
+            // assuming curr is already memory allocated...
+            struct xdll_node *next_pt = (struct xdll_node *)malloc(sizeof(struct xdll_node));
+            next_pt->elem = val;
+            next_pt->nxp = ptxor(curr_pt, NULL);
+            // https://stackoverflow.com/questions/26569728
+            // using-xor-with-pointers-in-c
+            curr_pt->nxp = ptxor(prev_pt, next_pt);
+            prev_pt = curr_pt;
+            curr_pt = next_pt;
+        }
+    }
+    return head_pt;
 }
 
 void display(struct xdll_node *node_pt)
 {
-   printf("%d -> ", node_pt->elem);
+    printf("%d -> ", node_pt->elem);
 }
 
 /*
- * Convience function for freeing nodes.
+ * Convince function for freeing nodes.
  */
 void free_xdll(struct xdll_node *node_pt)
 {
-   free(node_pt);
+    free(node_pt);
 }
 
 /*
@@ -93,16 +103,16 @@ void free_xdll(struct xdll_node *node_pt)
  */
 void iter(struct xdll_node *list, void (*iter_fn)(struct xdll_node *elem))
 {
-   struct xdll_node *prev_pt = NULL;
-   struct xdll_node *node_pt = list;
-   while (node_pt)
-   {
-      iter_fn(node_pt);
+    struct xdll_node *prev_pt = NULL;
+    struct xdll_node *node_pt = list;
+    while (node_pt)
+    {
+        iter_fn(node_pt);
 
-      struct xdll_node *next_pt = ptxor(prev_pt, node_pt->nxp);
-      prev_pt = node_pt;
-      node_pt = next_pt;
-   }
+        struct xdll_node *next_pt = (struct xdll_node *)((uintptr_t)prev_pt ^ (uintptr_t)node_pt->nxp);
+        prev_pt = node_pt;
+        node_pt = next_pt;
+    }
 }
 
 /*
@@ -111,42 +121,64 @@ void iter(struct xdll_node *list, void (*iter_fn)(struct xdll_node *elem))
  */
 struct xdll_node *rotate(struct xdll_node *head_pt)
 {
-   struct xdll_node* node;
+    struct xdll_node *prev_pt = NULL;
+    struct xdll_node *node_pt = head_pt;
+    while (node_pt)
+    {
+        uintptr_t next_pt = ((uintptr_t)prev_pt ^ (uintptr_t)node_pt->nxp);
+        prev_pt = node_pt;
+        node_pt = (struct xdll_node *)next_pt;
+    }
+    struct xdll_node *tail_pt = prev_pt;
 
+    struct xdll_node *forward = head_pt;
+    struct xdll_node *backwrd = tail_pt;
 
+    struct xdll_node *forward_prev = NULL;
+    struct xdll_node *backwrd_prev = NULL;
 
-   struct xdll_node *prev_pt = NULL;
-   struct xdll_node *node_pt = head_pt;
-   struct xdll_node *next_pt = ptxor(prev_pt, node_pt); 
-   do
-   {
-      next_pt = ptxor(prev_pt, node_pt); 
-      prev_pt = node_pt;
-      node_pt = next_pt;
-   } while (next_pt);
+    while (forward != backwrd)
+    {
+        struct xdll_node *forward_next_pt = (struct xdll_node *)
+                            ((uintptr_t)forward_prev ^ (uintptr_t)forward->nxp);
+        struct xdll_node *backwrd_next_pt = (struct xdll_node *)
+                            ((uintptr_t)backwrd_prev ^ (uintptr_t)backwrd->nxp);
 
-   head_pt = node_pt;
+        forward->elem = forward->elem ^ backwrd->elem;
+        backwrd->elem = backwrd->elem ^ forward->elem;
+        forward->elem = forward->elem ^ backwrd->elem;
 
-   printf("%p", head_pt);
-   
-   next_pt = NULL;
-   prev_pt = NULL;
+        forward_prev = forward;
+        backwrd_prev = backwrd;
 
-   do 
-   {
-      next_pt = ptxor(prev_pt, node_pt); 
-      prev_pt = node_pt;
-      node_pt = next_pt;
-   } while (next_pt); 
-
-   return head_pt;
+        forward = forward_next_pt;
+        backwrd = backwrd_next_pt;
+    }
+    return tail_pt;
 }
 
 int main(void)
 {
-   struct xdll_node *list = new_xdll(3, 100);
-   iter(list, display);
-   iter(rotate(list), display);
-   free_xdll(list);
-   return 0;
+    struct xdll_node *list = new_xdll(10, 100);
+
+    printf("iter(list, display):\n\t");
+    iter(list, display);
+    printf("\n\n");
+
+    printf("iter(rotate(list), display):\n\t");
+    iter(rotate(list), display);
+    printf("\n");
+
+    free_xdll(list);
+
+    return 0;
 }
+
+/**
+ * $ make &&./dll
+ * % iter(list, display):
+ * %      83 -> 86 -> 77 -> 15 -> 93 -> 35 -> 86 -> 92 -> 49 -> 21 -> 
+ * 
+ * % iter(rotate(list), display):
+ *        21 -> 49 -> 92 -> 86 -> 35 -> 93 -> 15 -> 77 -> 86 -> 83 ->
+*/
