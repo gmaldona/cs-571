2b571844f75a9ab42e4cd00da7bcd19be45961d7
Gregory Maldonado <gmaldona@oswego.edu>
2024-03-05 22:18:38 -0500

    automaton

--- earley-parser-java/src/AutomatonImpl.java
+++ earley-parser-java/src/AutomatonImpl.java
@@ -34,39 +34,62 @@ public class AutomatonImpl implements Automaton {
 
     @Override
     public void addState(int s, boolean is_start, boolean is_accept) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        if (is_start) {
+            start_states.add(s);
+        }
+        if (is_accept) {
+            accept_states.add(s);
+        }
     }
 
     @Override
     public void addTransition(int s_initial, char label, int s_final) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        StateLabelPair p = new StateLabelPair(s_initial, label);
+        if (! transitions.containsKey(p)) {
+            transitions.put(p, new HashSet<>());
+        }
+        transitions.get(p).add(s_final);
     }
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        this.current_states = start_states;
     }
 
     @Override
     public void apply(char input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        HashSet<Integer> new_states = new HashSet<>();
+        for (Integer state : current_states) {
+            StateLabelPair pair = new StateLabelPair(state, input);
+            for (StateLabelPair p : transitions.keySet()) {
+                if (pair.equals(p)) {
+                    new_states.addAll(transitions.get(p));
+                }
+            }
+        }
+        current_states = new_states;
     }
 
     @Override
     public boolean accepts() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        for (Integer state : current_states) {
+            if (accept_states.contains(state)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean hasTransitions(char label) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasTransitions'");
+        for (Integer state : current_states) {
+            StateLabelPair pair = new StateLabelPair(state, label);
+            for (StateLabelPair transition_pair : transitions.keySet()) {
+                if (pair.equals(transition_pair)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
-    
 }
