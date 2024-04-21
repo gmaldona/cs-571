2718bc3dd0fbc0e377606922d9f51ff82c4fa319
Gregory Maldonado <gmaldona@oswego.edu>
2024-04-21 17:25:32 -0400

    Hw4 pdf

--- haskell/bank.hs
+++ haskell/bank.hs
@@ -1,27 +1,30 @@
+data Bank = Bank Double deriving (Show)
 
+deposit :: Bank -> Double -> Double -> IO Bank
 
-
-
+deposit bank x = do
+   total <- a + x
+   return total
 
 -- Should output "(True, -50)"
-test1 =
-    (let bankOp = do
-            deposit 1000.0
-            withdraw 500.0
-            withdraw 550.0
-            b <- getBalance
-            o <- getOverdrawn
-            return (o, b)
-    in
-    runBankOp bankOp)
+-- test1 =
+--     (let bankOp = do
+--             deposit 1000.0
+--             withdraw 500.0
+--             withdraw 550.0
+--             b <- getBalance
+--             o <- getOverdrawn
+--             return (o, b)
+--     in
+--     runBankOp bankOp)
 
 
--- Should output "(200,-100)"
-test2 = 
-    (let bankOp = do
-            deposit 100.0
-            c <- withdraw 400.0
-            b <- getBalance
-            return (c, b)
-    in
-    runBankOp bankOp)
+-- -- Should output "(200,-100)"
+-- test2 = 
+--     (let bankOp = do
+--             deposit 100.0
+--             c <- withdraw 400.0
+--             b <- getBalance
+--             return (c, b)
+--     in
+--     runBankOp bankOp)
--- haskell/listManip.hs
+++ haskell/listManip.hs
@@ -1,6 +1,16 @@
+import Prelude
 
 prefix :: Eq a => [a] -> [a] -> Bool
-prefix = ???
+
+prefix [] list2 = True
+
+prefix list1 list2 = 
+    let h1 = head list1 in
+    let h2 = head list2 in
+            if h1 == h2 then 
+                prefix (tail list1) (tail list2)
+            else
+                False
 
 -- Should output "True"
 test1 = 
@@ -15,15 +25,13 @@ test2 =
     prefix input1 input2
 
 
-
-sublist :: [a] -> [[a]]
-sublist lst = ???
+sublist :: [Int] -> [[Int]]
+sublist [] = [[]]
+sublist (x:t) =
+    sublist t ++ [x:t' | t' <- sublist t]
 
 
 -- Should output "[[], [1], [2], [3], [1, 2], [2, 3], [1,3], [1, 2, 3]]"
 test3 =
     let input = [1,2,3] in
     sublist input
-
-
-
